1ff9ab72730fc069596d866ef15e04f14ade1b15
Alexei Bethinenko <vebsite100@yandex.ru>
2025-02-02 13:13:27 +0400

    Урок 10

---
+++ script.js
@@ -0,0 +1,41 @@
+const books = document.querySelectorAll('.book')
+const bg = document.querySelector('body')
+const title = document.querySelectorAll('a')
+const adv = document.querySelector('.adv')
+const ul = document.querySelectorAll('ul')
+const li = document.querySelectorAll('li')
+
+// Восстановить порядок книг.
+
+
+
+books[0].before(books[1])
+books[4].after(books[3])
+books[5].after(books[2])
+console.log(books)
+
+// Заменить картинку заднего фона на другую из папки image
+bg.style.backgroundImage = 'url(./image/adv.jpg)';
+
+// Исправить заголовок в книге 3( Получится - "Книга 3. this и Прототипы Объектов")
+title[4].textContent = 'Книга 3. this и Прототипы Объектов'
+console.log(title)
+// Удалить рекламу со страницы
+adv.remove()
+
+// Восстановить порядок глав во второй и пятой книге (внимательно инспектируйте индексы элементов, поможет dev tools)
+li[9].after(li[2])
+li[3].after(li[6])
+li[4].before(li[8])
+
+li[49].before(li[55])
+li[52].before(li[48])
+li[54].before(li[51])
+
+console.log(ul)
+console.log(li)
+
+// в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
+const cloneEleme =  li[25].cloneNode()
+cloneEleme.textContent = 'Глава 8: За пределами ES6'
+li[25].after(cloneEleme)
